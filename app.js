2bffec26c093990defa0a17d98a177915d8751a8
AlexNazaroff <ale.krotov@gmail.com>
2021-04-07 20:51:44 +0500

    npm install express mongoose npm install -D nodemon concurrently npm i config

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('App')
+const express = require('express')
+const config = require('config')
+const path = require('path')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
