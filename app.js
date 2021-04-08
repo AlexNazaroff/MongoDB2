3301a1612ce8bf988849ea9097b4ef0a66f68fe3
AlexNazaroff <ale.krotov@gmail.com>
2021-04-08 11:17:44 +0500

    user

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const mongoose = require('mongoose')
 
 const app = express()
 
+app.use('/api/auth', require('./routes/auth.routes'))
+
     /*app.use(express.json({ extended: true }))
 
-    app.use('/api/auth', require('./routes/auth.routes'))
     app.use('/api/link', require('./routes/link.routes'))
     app.use('/t', require('./routes/redirect.routes'))
 
